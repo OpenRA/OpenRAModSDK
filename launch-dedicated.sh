50e5c4b34267284bcea62deed0b6bc5f259d37e7
Paul Chote <pchote@users.noreply.github.com>
2017-05-29 12:44:44 +0100

    Fix posix compatibility and code style of shell scripts.

--- launch-dedicated.sh
+++ launch-dedicated.sh
@@ -8,27 +8,26 @@
 
 TEMPLATE_LAUNCHER=$(python -c "import os; print(os.path.realpath('$0'))")
 TEMPLATE_ROOT=$(dirname "${TEMPLATE_LAUNCHER}")
-source ${TEMPLATE_ROOT}/mod.config
+. "${TEMPLATE_ROOT}/mod.config"
 
 MOD_SEARCH_PATHS="${TEMPLATE_ROOT}/mods"
 if [ "${INCLUDE_DEFAULT_MODS}" = "True" ]; then
 	MOD_SEARCH_PATHS="${TEMPLATE_PATHS},./mods"
 fi
 
+NAME="${Name:-"Dedicated Server"}"
 LAUNCH_MOD="${Mod:-"${MOD_ID}"}"
-
-Name="${Name:-"Dedicated Server"}"
-Mod="${Mod:-"${MOD_ID}"}"
-ListenPort="${ListenPort:-"1234"}"
-ExternalPort="${ExternalPort:-"1234"}"
-AdvertiseOnline="${AdvertiseOnline:-"True"}"
-EnableSingleplayer="${EnableSingleplayer:-"False"}"
-Password="${Password:-""}"
+LISTEN_PORT="${ListenPort:-"1234"}"
+EXTERNAL_PORT="${ExternalPort:-"1234"}"
+ADVERTISE_ONLINE="${AdvertiseOnline:-"True"}"
+ENABLE_SINGLE_PLAYER="${EnableSingleplayer:-"False"}"
+PASSWORD="${Password:-""}"
 
 cd engine
+
 while true; do
-     MOD_SEARCH_PATHS="${MOD_SEARCH_PATHS}" mono --debug OpenRA.Server.exe Game.Mod=$Mod \
-     Server.Name="$Name" Server.ListenPort=$ListenPort Server.ExternalPort=$ExternalPort \
-     Server.AdvertiseOnline=$AdvertiseOnline \
-     Server.EnableSingleplayer=$EnableSingleplayer Server.Password=$Password
+     MOD_SEARCH_PATHS="${MOD_SEARCH_PATHS}" mono --debug OpenRA.Server.exe Game.Mod="${LAUNCH_MOD}" \
+     Server.Name="${NAME}" Server.ListenPort="${LISTEN_PORT}" Server.ExternalPort="${EXTERNAL_PORT}" \
+     Server.AdvertiseOnline="${ADVERTISE_ONLINE}" \
+     Server.EnableSingleplayer="${ENABLE_SINGLE_PLAYER}" Server.Password="${PASSWORD}"
 done
