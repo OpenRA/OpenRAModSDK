817bffb4237359c8d7c21f1a93f8938dc64482b7
Paul Chote <pchote@users.noreply.github.com>
2017-07-15 15:02:14 +0000

    Add Windows and macOS packaging scripts.

---
+++ packaging/package-all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne "2" ]; then
+    echo "Usage: `basename $0` version outputdir"
+    exit 1
+fi
+
+# Set the working dir to the location of this script
+cd "$(dirname $0)"
+
+pushd windows >/dev/null
+echo "Building Windows package"
+./buildpackage.sh "$1" "$2"
+if [ $? -ne 0 ]; then
+    echo "Windows package build failed."
+fi
+popd >/dev/null
+
+pushd osx >/dev/null
+echo "Building macOS package"
+./buildpackage.sh "$1" "$2"
+if [ $? -ne 0 ]; then
+    echo "macOS package build failed."
+fi
+popd >/dev/null
+
+echo "Package build done."
